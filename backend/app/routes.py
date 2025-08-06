2cb2d3f2217acb7501c434a64588b61fbaa1ae90
ortizogaima <ortizogaimanol.labase@gmail.com>
2025-08-06 12:05:37 -0300

    Revert "create project"
    This reverts commit 93ce33ec27c6731edea5e53742581363bffe98d9.

--- backend/app/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-api = Blueprint('api', __name__)
-
-@api.route('/api/hello', methods=['GET'])
-def hello():
-    return jsonify({"mensaje": "Hola desde Flask!"})
